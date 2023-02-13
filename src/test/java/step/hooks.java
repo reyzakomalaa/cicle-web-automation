e7fedabf621e5067f187488b626bfcde2ff76a3b
reyzakomalaa <reyza.pratama1001@gmail.com>
2023-02-13 17:35:25 +0700

    set up web automation

--- src/test/java/step/hooks.java
+++ src/test/java/step/hooks.java
@@ -3,13 +3,17 @@ package step;
 import config.env;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class hooks extends env {
     @Before
     public void before(){
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
-        driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions opt = new ChromeOptions();
+        opt.setHeadless(false);
+        driver = new ChromeDriver(opt);
         driver.get("");
         driver.manage().window().maximize();
     }
