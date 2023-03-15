1adbd80cac9f85ce1c097e2269dad3ec0fff9a8d
reyzakomalaa <reyza.pratama1001@gmail.com>
2023-03-15 23:35:38 +0700

    Initial commit

---
+++ src/test/java/config/env.java
@@ -0,0 +1,9 @@
+package config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class env {
+    protected static WebDriver driver;
+    protected static WebDriverWait wait;
+}
